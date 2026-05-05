2117e573541c8866808e28ddc181d12bc39226f3
Faeq-F <faeqfaisal@hotmail.co.uk>
2026-05-05 07:53:25 +0000

    Deploying to gh-pages from  @ b6d24ab3697ec3c9e2f7b00de215e42c4b40dcde 🚀

---
+++ assets/CQKHc085.js
@@ -0,0 +1,1 @@
+import{cM as o,z as r,h as u}from"./BgpQ0OgC.js";function a(e){const n=o({locale:r("en")});return u(()=>{var t;return(e==null?void 0:e.value)||((t=n.locale)==null?void 0:t.value)||"en"})}export{a as u};
